fce4a7a9a8c81a0d4ca67d3ae6b0471126301032
Sabrina <sabrinah_38@hotmail.fr>
2026-06-04 14:31:59 +0200

    refactor authRouter registration logic for improved validation and error handling

--- api-voitures/src/router/auth.ts
+++ api-voitures/src/router/auth.ts
@@ -9,40 +9,52 @@ const prisma = new PrismaClient();
 export const authRouter = Router();
 
 authRouter.post("/local/register", async (req, res) => {
-    const { motdpasse, pseudo } = req.body;
-    const userWithpseudo = await prisma.user.findFirst({ where: { pseudo } });
-    if (userWithpseudo) {
-        res.status(400).json("pseudo already exists");
-    }
-    else {
-        const hashedmotdpasse = await bcrypt.hash(motdpasse, parseInt(process.env.SALT_ROUNDS!));
-        const newUser = await prisma.user.create({ 
-            data: {
-                motdpasse: hashedmotdpasse, 
-                pseudo
-            } 
-            });
-        res.json(newUser);
-    }
+  const { motdepasse, pseudo } = req.body.data;
+
+  if (!motdepasse || !pseudo) {
+    return res.status(400).json({
+      message: "Pseudo et mot de passe obligatoires",
+    });
+  }
+
+  const userWithpseudo = await prisma.user.findUnique({ where: { pseudo } });
+
+  if (userWithpseudo) {
+    res.status(200).json("Pseudo et mot de passe obligatoires invalide");
+  } else {
+    const hashedmotdpasse = await bcrypt.hash(
+      motdepasse,
+      parseInt(process.env.SALT_ROUNDS!),
+    );
+
+    const newUser = await prisma.user.create({
+      data: {
+        pseudo,
+        motdepasse: hashedmotdpasse,
+      },
+    });
+    res.status(201).json(newUser);
+  }
 });
 
 authRouter.post("/local", async (req, res) => {
-    const { pseudo, motdpasse } = req.body;
-    const userWithpseudo = await prisma.user.findFirst({ where: { pseudo } });
-    if (!userWithpseudo) {
-        res.status(400).json("pseudo is incorrect");
+  const { pseudo, motdpasse } = req.body;
+  const userWithpseudo = await prisma.user.findFirst({ where: { pseudo } });
+  if (!userWithpseudo) {
+    res.status(400).json("pseudo is incorrect");
+  } else {
+    const ismotdpasseCorrect = await bcrypt.compare(
+      motdpasse,
+      userWithpseudo.motdpasse,
+    );
+    if (ismotdpasseCorrect) {
+      const token = jwt.sign(userWithpseudo, process.env.JWT_SECRET!);
+      res.json({
+        token,
+        ...userWithpseudo,
+      });
+    } else {
+      res.status(400).json("motdpasse is incorrect");
     }
-    else {
-        const ismotdpasseCorrect = await bcrypt.compare(motdpasse, userWithpseudo.motdpasse);
-        if (ismotdpasseCorrect) {
-            const token = jwt.sign(userWithpseudo, process.env.JWT_SECRET!);
-            res.json({
-                token,
-                ...userWithpseudo
-            });
-        }
-        else {
-            res.status(400).json("motdpasse is incorrect");
-        }
-    }
-})
+  }
+});
