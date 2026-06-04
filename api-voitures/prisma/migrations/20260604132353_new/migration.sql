fd9de0a63b687d76807caa911d1220f363ca2e41
Sabrina <sabrinah_38@hotmail.fr>
2026-06-04 15:39:49 +0200

    add initial database migration and lock files for Voiture and User tables

---
+++ api-voitures/prisma/migrations/20260604132353_new/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Voiture" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "pseudo" TEXT NOT NULL,
+    "motdpasse" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_pseudo_key" ON "User"("pseudo");
